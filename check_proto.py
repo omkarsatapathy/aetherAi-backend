19668c9624ec3391912eb7d97e0e0e8f20d22a1d
omkarsatapathy <omkarsatapathy001@gmail.com>
2025-11-27 00:59:09 +0530

    Few changes for New repo in fronend

--- check_proto.py
+++
@@ -1,5 +0,0 @@
-import google.generativeai as genai
-try:
-    print(dir(genai.protos.Tool))
-except:
-    print("Could not dir genai.protos.Tool")
--- check_protos_root.py
+++
@@ -1,5 +0,0 @@
-import google.generativeai as genai
-try:
-    print(dir(genai.protos))
-except:
-    print("Could not dir genai.protos")
